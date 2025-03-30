b79681f17178d71bf91eaba2d7b13729977a7e38
MNS <sanjayajnas447@gmail.com>
2025-03-30 19:40:27 +0530

    Create Optimal Solution - Two pointers.py

---
+++ Optimal Solution - Two pointers.py
@@ -0,0 +1,17 @@
+'''
+Algorithm :
+  Use two pointers: left and right to traverse the array.
+  Track the last seen nonzero element in left pointer.
+  If an opposite fort is found (forts[left] == -forts[right]), update the maximum captured forts.
+  Update left to right for the next potential capture.
+  Note: Guys dry run and do manual calculations for testcase forts -> forts = [1,0,0,-1,0,0,0,0,1] and you'll understand it better.
+'''
+def captureForts(self, forts):   #You should ignore the self parameter if you do not refer to any class instances guys, remember that.   
+      left = ans = 0
+      n = len(forts)
+      for right in range(1, n) :
+          if forts[right] :
+              if forts[left] == -forts[right] :   
+                  ans = max(ans, right-left-1)
+              left = right
+      return ans
